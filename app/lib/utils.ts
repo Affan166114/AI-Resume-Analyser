aa757a20cfae5af4add807a2f5c32c40117c9436
S Md Affan <newoffiacc@gmail.com>
2026-01-16 21:53:23 +0530

    Resume Feedback Page

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,16 +1,21 @@
-/**
- * Format file size
- * @param bytes 
- * @returns 
- */
-export function formatSize(bytes: number): string {
-    if(bytes===0)
-            return '0 Bytes';
-    const k=1024;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    
-    const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(k))));
-    return Math.round(bytes / Math.pow(k,i)) + ' ' + sizes[i];
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
 
-export const generateUUID =()=> crypto.randomUUID();
+export function formatSize(bytes: number): string {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+  // Determine the appropriate unit by calculating the log
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  // Format with 2 decimal places and round
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
+
+export const generateUUID = () => crypto.randomUUID();
